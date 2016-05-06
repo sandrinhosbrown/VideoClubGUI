86e33019ef39fad15a23d0931c9e2bf9b8929827
sandrinhosbrown <sandrinhosbrown@gmail.com>
2016-05-06 08:54:55 +0200

    Preparado menu principal. Inicializadas variables misPeliculas y miFichero en la clase Main.

--- src/videoclubfichero/VideoClubFichero.java
+++ src/videoclubfichero/VideoClubFichero.java
@@ -5,17 +5,29 @@
  */
 package videoclubfichero;
 
+import utilidades.Fichero;
+
 /**
  *
  * @author usu21
  */
 public class VideoClubFichero {
+    public static Fichero miFichero;
+    public static ListaPeliculas misPeliculas;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        //Inicializamos el fichero y la lista de peliculas
+        miFichero = new Fichero ("videoclub.xml");
+        misPeliculas = (ListaPeliculas) miFichero.leer(); //casting de objeto q devuelve leer() a ListaPeliculas
+        if(misPeliculas == null){
+            misPeliculas = new ListaPeliculas();
+        }
+        //Creamos el Menu Principal y lo hacemos visible
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.setVisible(true);
     }
     
 }
